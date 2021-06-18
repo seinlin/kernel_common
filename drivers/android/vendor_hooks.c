222d77c11bada28d20a38fdbfd94343153cffe2f
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-06-18 13:28:11 +0800

    ANDROID: syscall_check: add vendor hook for open syscall
    Through this vendor hook, we can get the timing to check
    current running task for the validation of its credential
    and open operation.
    
    Bug: 191291287
    
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: Ia644ceb02dbc230ee1d25cad3630c2c3f908e41a
    (cherry picked from commit a7a3b31d5860bead8eec9e72a333351b8c2ac1ca)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -79,4 +79,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_node_replace);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_lookup);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_is_initialized);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_file_open);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
