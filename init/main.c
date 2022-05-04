168a4fcb1c76321d315db08d8df818cb37003eea
Joe Fradley <joefradley@google.com>
2022-05-04 23:00:45 +0000

    ANDROID: GKI: Disable KUnit built-in testing
    Permanently disable running built-in KUnit tests.
    
    Squash:
      ANDROID: GKI: Add ‘kunit.enable’ module parameter
    
    Bug: 231160059
    Bug: 231160619
    Signed-off-by: Joe Fradley <joefradley@google.com>
    Change-Id: Ic0610b14b8b84fbedbe5ae813555f0856941e1a3
    Signed-off-by: Lee Jones <joneslee@google.com>

--- init/main.c
+++ init/main.c
@@ -111,8 +111,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/initcall.h>
 
-#include <kunit/test.h>
-
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -1569,8 +1567,6 @@ static noinline void __init kernel_init_freeable(void)
 	page_alloc_init_late();
 
 	do_basic_setup();
-
-	kunit_run_all_tests();
 
 	wait_for_initramfs();
 	console_on_rootfs();
